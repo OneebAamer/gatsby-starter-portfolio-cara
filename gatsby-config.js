b79c21002048f9dade9122efa432fd2b0c2eac41
Oneeb Aamer <oneeb47@gmail.com>
2022-06-05 16:45:47 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
     siteTitle: `Oneeb Aamer`,
-    siteTitleAlt: `Oneeb Aamer's Portfolio`,
+    siteTitleAlt: `Oneeb Aamer`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
